f86903c0b26986bad0f0b22df0a9e71c0de4c5bc
Antony Rithick <148781725+antonyrithick@users.noreply.github.com>
2024-12-28 21:07:28 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 const sequelize = new Sequelize('bhqwtlwhr9la38iz8smi', 'uaprqecbissc4pr4', 'vGIkdhqn5KjKTps1gaJP', {
     host: 'bhqwt1whr9la38iz8smi-mysql.services.clever-cloud.com',
     dialect: 'mysql',
-    port: 3306,
+  port: process.env.DB_PORT || 3306, 
+  logging: false,
 });
 
 sequelize
